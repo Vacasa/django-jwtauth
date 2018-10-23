06ab415212ecbe787a7e0ae59713dfdfe7058fcb
Morgan Denning <morgan.denning@vacasa.com>
2018-10-23 10:05:28 -0700

    add verify

---
+++ django_jwtauth/verify.py
@@ -0,0 +1,35 @@
+import jwt
+import requests
+
+from time import time
+from haslib import sha256
+
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+from django.conf import settings
+from django.core.cache import caches
+from django.core.exceptions import PermissionDenied
+
+from .utils import get_public_key
+
+
+def verify_token(self, request, *args, **kwargs):
+    if 'HTTP_AUTHORIZATION' not in request.META:
+        raise PermissionDenied
+
+        bearer = request.META['HTTP_AUTHORIZATION'].split(' ')[1]
+
+    try:
+        claims = jwt.decode(
+            bearer,
+            key=get_public_key(),
+            algorithms=[settings.DJANGO_JWTAUTH['JWT_ALGORITHM']],
+            *args,
+            **kwargs,
+        )
+
+        return claims
+    except (
+        jwt.PyJWTError
+    ):
+        raise PermissionDenied(str(e))
